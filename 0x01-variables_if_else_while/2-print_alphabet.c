d33d0781f03807f1f2abd39831a22eb8089eb8e5
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-03-17 17:03:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints alaphabets in lower case
+ * Return: Always
+ *
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
