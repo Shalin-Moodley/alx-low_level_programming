1c7f77fdd63dec549f944ee0c08a98425b20d3f5
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-03-23 12:17:41 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - Checks uppercase
+ * @x: Number to be checked
+ * Return: 1 for upper, 0 for rest
+ */
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
