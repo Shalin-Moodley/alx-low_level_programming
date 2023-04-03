fd551f5f751b08166a21a2357d77f5a67bf2af02
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-04-03 15:49:35 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - entry
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
+	return (dest);
+}
