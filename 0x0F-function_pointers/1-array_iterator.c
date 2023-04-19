dbe34d5bafa025e1f539ae79b0b0b80aad1320d4
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-04-19 15:28:29 -0700

    1-array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iterates array
+ * @array: array
+ * @size: size of array
+ * @action: pointer
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (x < size)
+		{
+			action(array[x]);
+			x++;
+		}
+	}
+}
