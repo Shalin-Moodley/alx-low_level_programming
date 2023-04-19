79ddf3b1d9a82cd713f63fd5583c40e0a9eb31a8
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-04-19 15:07:59 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Prints name
+ * @name: Name to be printed
+ * @f: pointer
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
