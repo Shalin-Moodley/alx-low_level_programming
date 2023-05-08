153eb32170c1e03b43209ed5aa761f356a6dea87
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-05-08 13:58:37 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,6 +6,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer
+ * @text_content: The string to add
+ * Return: If the function fails or filename is NULL - -1
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
