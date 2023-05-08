49a285e3c6c196c2a32068d98adc2e508bae9a5d
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-05-08 13:55:39 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd = open(filename, O_WRONLY | O_APPEND);
+
+if (filename == NULL)
+{
+return (-1);
+}
+if (fd == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+ssize_t len = strlen(text_content);
+ssize_t bytes_written = write(fd, text_content, len);
+if (bytes_written != len)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
